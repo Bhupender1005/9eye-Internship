73b97568c8d36a33f0bd0ebec04e133569892fde
sanyaswain <sanyaswain16@gmail.com>
2020-06-26 03:24:06 +0530

    Added multiscreen navigation and a new Book Now screen (which can be accessed from the drawer)

--- lib/booking.dart
+++ lib/booking.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:coming_soon/coming_soon.dart';
 
 void main() => runApp(MaterialApp(
-      home: ComingSoon(),
+      home: Booking(),
     ));
 
-class ComingSoon extends StatelessWidget {
+class Booking extends StatefulWidget {
+  @override
+  _BookingState createState() => _BookingState();
+}
+
+class _BookingState extends State<Booking> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  
+
+  void _gotoProduct() {
+    Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => ComingSoon(),
+        ),
+        );
+  }
+  void _gotoBooking() {
+    Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => Booking(),
+        ),
+        );
+  }
+
+  DateTime _date;
+  TimeOfDay _time;
+
+  List<Color> _colors = [Colors.purple[100], Colors.purple[600]];
+  List<double> _stops = [0.0, 0.9];
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +133,7 @@ class ComingSoon extends StatelessWidget {
 
             InkWell(
                 onTap: (){
-                    Navigator.of(context).pop();
+                    _gotoBooking();
                 },
                 splashColor: Colors.purple[100],
                 highlightColor: Colors.purple[100],
@@ -150,7 +185,7 @@ class ComingSoon extends StatelessWidget {
 
             InkWell(
                 onTap: (){
-                    Navigator.of(context).pop();
+                    _gotoProduct();
                 },
                 splashColor: Colors.purple[100],
                 highlightColor: Colors.purple[100],
@@ -211,73 +246,147 @@ class ComingSoon extends StatelessWidget {
         )),
       ),
 
-      body: Container(
-          child: Column(
-              children: <Widget>[
-                  SizedBox(height: 70.0),
-                  Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 45, 0),
-                      child: Text(
-                        'Welcome to 9eye!',
-                        style: TextStyle(
-                            fontSize: 40,
-                            fontFamily: 'Gilroy-Bold',
-                            fontWeight: FontWeight.bold
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 35.0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(35, 0, 20, 0),
+              child: Row(
+                  children: <Widget>[
+                      Text(
+                          _date == null ? 'Select a Date' : 'Booking Date: ${DateFormat.yMMMd().format(_date)}',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontFamily: 'Gilroy-Regular',
+                            fontWeight: FontWeight.normal
                         ),
-                        textAlign: TextAlign.start,
-                    ),
-                  ),
-                  SizedBox(height: 50.0),
-                  Container(
-                      height: 328,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/shopping.png'),
-                              fit: BoxFit.fill
-                          )
-                      )
-                  ),
-                  SizedBox(height: 40.0),
-                  Container(
-                      decoration: BoxDecoration(
-                          color: Colors.purple[600],
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                              BoxShadow(
-                              color: Colors.purple[900],
-                              blurRadius: 20,
-                              offset: Offset(0, 10)
-                          )],
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Text(
-                            "We're coming soon!",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Gilroy-Regular',
-                                fontSize: 25,
-                                wordSpacing: 1.5,
-                            ),
+                      //${_dateTime.day}/${_dateTime.month}/${_dateTime.year}
 
+                      SizedBox(width: 30),
+                      IconButton(
+                          icon: Icon(
+                          Icons.calendar_today,
+                          size: 30,
                             ),
-                      ),
-                  ),
-              ],
+                            alignment: Alignment.center,
+                            color: Colors.purple[600],
+                            onPressed: () {
+                          showDatePicker(
+                              context: context, 
+                              initialDate: _date == null ? DateTime.now() : _date, 
+                              firstDate: DateTime(2020), 
+                              lastDate: DateTime(2021)
+                              ).then((date) {
+                                  setState(() {
+                                    _date = date;
+                                  });
+                              });
+                            }
+                            ),
+                  ]
               ),
+            ),
+
+            SizedBox(height: 40),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(35, 0, 20, 0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                              _time == null ? 'Select a Time' : "Booking Time: ${_time.format(context)}",
+                              style: TextStyle(
+                              fontSize: 25,
+                              fontFamily: 'Gilroy-Regular',
+                              fontWeight: FontWeight.normal
+                          ),
+                          ),
+                  
+                  SizedBox(width: 25),
+                  IconButton(
+                          icon: Icon(
+                              Icons.alarm,
+                              size: 35,
+                          ),
+                          alignment: Alignment.center,
+                          color: Colors.purple[600],
+                          onPressed: () {
+                              showTimePicker(
+                                  context: context, 
+                                  initialTime: _time == null ? TimeOfDay.now() : _time
+                                  ).then((time) {
+                                      setState(() {
+                                        _time = time;
+                                      });
+                                  });
+                          }
+                          )
+                ],
+              ),
+            ),
+
+            SizedBox(height: 40),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(35, 0, 0, 0),
+              child: Text(
+                  "No. of Persons",
+                  style: TextStyle(
+                                fontSize: 25,
+                                fontFamily: 'Gilroy-Regular',
+                                fontWeight: FontWeight.normal
+                            ),
+                  textAlign: TextAlign.start,
+              ),
+            ),
+            
+            SizedBox(height: 18),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+              child: FlutterRatingBar(
+                  itemCount: 5,
+                  fillColor: Colors.purple[600],
+                  borderColor: Colors.purple,
+                  onRatingUpdate: (rating) {
+                      print(rating);
+                      },
+                  ),
+            ),
+
+            SizedBox(height: 270),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(70, 0, 70, 0),
+              child: Container(
+                  alignment: Alignment(0, 0),
+                        decoration: BoxDecoration(
+                            color: Colors.purple[600],
+                            gradient: LinearGradient(
+                                colors: _colors,
+                                stops: _stops,
+                                ),
+                            borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                              "Book Appointment",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Gilroy-Regular',
+                                  fontSize: 25,
+                                  wordSpacing: 1.5,
+                              ),
+
+                              ),
+                        ),
+                    ),
+            ),
+        ],
       ),
-      //body: Center(
-        //child: Image.asset('assets/shopping.png'),
-        // child: Text(
-        //     "We're coming soon!",
-        //     style: TextStyle(
-        //                       color: Colors.pink[900],
-        //                       fontSize: 38,
-        //                       fontFamily: 'Gilroy-Bold',
-        //                       fontWeight: FontWeight.bold
-        //                   ),
-        //                   ),
-      //),
+
       bottomNavigationBar:
           BottomNavigationBar(
               type: BottomNavigationBarType.fixed,
